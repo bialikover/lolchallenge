b364ad3746dc969a61ae96194e76b25a70875d83
Adán G. Galván González <bialikoer@gmail.com>
2014-05-08 17:06:05 -0500

    add share, and save summoners in db

--- app/routes.js
+++ app/routes.js
@@ -12,10 +12,47 @@ module.exports = function(app) {
         res.render('index.ejs'); // load the index.ejs file
     });
 
+    // =====================================
+    // Insert summoner on db ===============
+    // =====================================
+
     app.post('/join', function(req, res) {
         console.log(req.body);
+        var user_data = {
+            fbid: req.body.user.fbid,
+            name: req.body.user.name,
+            email: req.body.user.email,
+            summoner: req.body.user.summoner
+        };
+
+        var user = new User(user_data);
+
+        user.save(function(error, data) {
+            if (error) {
+                res.json(error);
+            } else {
+                res.json(data);
+            }
+        });
     });
 
+    // =====================================
+    // Verify if a summoner exists =========
+    // =====================================
+    app.get('/summoner', function(req, res) {
+        var user = JSON.parse(req.query.user);
+        console.log(user);
+        User.findOne({
+            fbid: user.fbid
+        }, function(err, obj) {
+            console.log(obj);
+            res.json(obj);
+        });
+    });
+
+    // =====================================
+    // List all summoners ==================
+    // =====================================
     app.get('/summoners', function(req, res) {
         User.find({}, function(users) {
             res.json(users);
--- views/js/app.js
+++ views/js/app.js
@@ -1,55 +1,152 @@
-window.fbAsyncInit = function() {
-  FB.init({
-    appId      : '685626324807687',
-    cookie     : true,  // enable cookies to allow the server to access 
-                        // the session
-    xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.0' // use version 2.0
-  });
-FB.getLoginStatus(function(response) {
-        if (response.status === 'connected') {
-            checkifLikes();
-            getUserInfo();
-        }else {
-            FB.login(function(){
-               checkifLikes();
-                getUserInfo(); 
-            }, {scope: 'email publish_actions user_likes'});
-        }
+$(document).ready(function() {
+    window.user = {};
+    $(".wait").show();
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId: '685626324807687',
+            cookie: true, // enable cookies to allow the server to access 
+            // the session
+            xfbml: true, // parse social plugins on this page
+            version: 'v2.0' // use version 2.0
+        });
+        FB.getLoginStatus(function(response) {
+            if (response.status === 'connected') {
+                getUserInfo();
+
+            } else {
+                FB.login(function() {
+                    getUserInfo();
+
+                }, {
+                    scope: 'email publish_actions user_likes'
+                });
+            }
+        });
+    }
+
+    function checkifLikes() {
+        FB.api({
+            method: 'pages.isFan',
+            page_id: '1470973119803425'
+        }, function(resp) {
+            if (resp) {
+                console.log('You like it lolpuebla.');
+                hideAll();
+                whichLanding(user);
+            } else {
+                hideAll();
+                $("#paso1").fadeIn();
+                console.log("You don't like it.");
+            }
+        });
+    }
+
+    function whichLanding(user) {
+        $.ajax({
+            url: "/summoner",
+            data: {
+                "user": JSON.stringify(user)
+            },
+            type: 'GET',
+            dataType: "JSON"
+        }).success(function(json) {
+            if (json) {
+                hideAll();
+                $("#user").text(json.name);
+                $("#summoner").text(json.summoner);
+                $("#congrats").show();
+            } else {
+                hideAll();
+                $("#paso2").show();
+            }
+        });
+    }
+
+    function hideAll() {
+        $(".wait").hide();
+        $("#paso1").hide();
+        $("#paso2").hide();
+        $("#congrats").hide();
+    }
+
+    function getUserInfo() {
+        FB.api("/me", function(response) {
+            if (response && !response.error) {
+                user = {
+                    "fbid": response.id,
+                    "name": response.name,
+                    "email": response.email
+                }
+                checkifLikes();
+            }
+        });
+    }
+
+    function postToFacebook() {
+        var share = {
+            method: 'stream.share',
+            u: 'http://www.facebook.com/lolpuebla'
+        };
+        FB.ui(share, function(response) {
+            if (response) {
+                saveSummoner();
+            }
+        });
+    }
+
+    function saveSummoner() {
+        user.summoner = $("#summoner-name").val();
+        summonerExists(user);
+    }
+
+    function summonerExists(user) {
+        hideAll();
+        $(".wait").show();
+        $.ajax({
+            url: "/summoner",
+            data: {
+                "user": JSON.stringify(user)
+            },
+            type: 'GET',
+            dataType: "JSON"
+        }).success(function(json) {
+            if (json) {
+                hideAll();
+                $("#user").text(json.name);
+                $("#summoner").text(json.summoner);
+                $("#congrats").show();
+            } else {
+                insertSummoner(user);
+            }
+        });
+        //AJAX REQUEST
+    }
+
+    function insertSummoner(user) {
+        $.ajax({
+            url: "/join",
+            data: {
+                "user": user
+            },
+            type: 'POST',
+            dataType: "JSON"
+        }).success(function(json) {
+            if (json) {
+                console.log(json);
+                hideAll();
+                $("#user").text(json.name);
+                $("#summoner").text(json.summoner);
+                $("#congrats").show();
+            } else {
+                console.log("error");
+                console.log(json);
+            }
+        });
+    }
+    //Listeners:
+    $("#share").click(function(e) {
+        e.preventDefault();
+        postToFacebook();
     });
-}
 
-function checkifLikes(){
-    FB.api({ method: 'pages.isFan', page_id: '1470973119803425' }, function(resp) {
-    if (resp) {
-      console.log('You like it lolpuebla.');
-      hideAll();
-      $("#paso2").fadeIn();
-    } else {
-      hideAll();
-      $("#paso1").fadeIn();
-      console.log("You don't like it.");
-    }
-  });
-}
-
-function hideAll(){
-    $(".loader").hide();
-    $("#paso1").hide();
-    $("#paso2").hide();
-    $("#congrats").hide();
-}
-
-function getUserInfo(){
-    FB.api("/me",function (response) {
-      if (response && !response.error) {
-        var user = {
-            "fbid": response.id,
-            "name": response.name,
-            "email": response.email
-        }
-        console.log(user);
-      }
-    }
-);
-}
+});
